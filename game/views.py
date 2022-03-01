33e23ddf7ef7350d22b5b6fe0e2d971a12bc7b7e
sxx200713 <sxx200713@163.com>
2022-03-01 13:43:16 +0800

    edition1

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src="https://tse1-mm.cn.bing.net/th/id/R-C.1a6e835c9e2a41d6fbf33ab0d0938f02?rik=s25%2bcT3Mf8HZjw&riu=http%3a%2f%2fwww.desktx.com%2fd%2ffile%2fwallpaper%2fgame%2f20161108%2feb7254c8de337fd7aa528b48129f34d7.jpg&ehk=7Lf85lGP2FPf4DhC29dqaBPHtqEOVcTH2rvCzK4Z8CA%3d&risl=&pid=ImgRaw&r=0%E3%80%81">'
+    return HttpResponse(line1+line2)
+
