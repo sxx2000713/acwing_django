f576b9893128fec1ecd73220eec8eb102cbc16e7
sxx200713 <sxx200713@163.com>
2022-05-02 14:49:51 +0800

    create structure

---
+++ scripts/compress_game_js.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+JS_PATH=/home/sxx_django/acapp/game/static/js/
+JS_PATH_FINAL=${JS_PATH}final
+JS_PATH_SRC=${JS_PATH}src
+
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_FINAL}game.js
